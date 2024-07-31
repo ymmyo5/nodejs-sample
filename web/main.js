302de9ad57d44c819a091c0f6fa237f8a997b3ac
ymmyo5 <epc1126@yahoo.co.jp>
2024-07-31 14:42:44 +0900

    カウント処理追加

--- web/main.js
+++ web/main.js
@@ -1,1 +1,31 @@
 console.log("Hello world");
+
+window.addEventListener("load", function(){
+    let count = 0;
+    const plusButton = document.getElementById("plus-button");
+    const countPreview = document.getElementById("count-preview");
+    const minusButton = this.document.getElementById("minus-button");
+    console.log(plusButton);
+
+    function handlePlus(){
+        console.log("click");
+        count++;
+        console.log(count);
+        console.dir(countPreview);
+        countPreview.innerHTML = count;
+        console.dir(countPreview);        
+    }
+
+    function handleMinus(){
+        if(count == 0){
+            return;
+        }
+        console.log("minus");
+        count--;
+        console.log(count);
+        countPreview.innerHTML = count;
+    }
+
+    plusButton.addEventListener("click", handlePlus)
+    minusButton.addEventListener("click", handleMinus)
+})
